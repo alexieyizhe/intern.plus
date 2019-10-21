c4c9179e6bcd89c576b72c22c7efed62862ad29b
Alex Xie <alexieyizhe@gmail.com>
2019-10-21 10:14:56 -0400

    make carousel more obv

--- src/pages/landing/components/LandingCardDisplay.tsx
+++ src/pages/landing/components/LandingCardDisplay.tsx
@@ -13,7 +13,15 @@ import {
 } from "src/types";
 import pageCopy from "../copy";
 
-import { CompanyCard, ReviewCard, Link, Text } from "src/components";
+import {
+  CompanyCard,
+  ReviewCard,
+  Link,
+  Text,
+  HEADER_PADDING,
+  HEADER_PADDING_MOBILE,
+  MOBILE_MENU_MEDIA_QUERY,
+} from "src/components";
 
 /*******************************************************************
  *                             **Types**                           *
@@ -122,6 +130,15 @@ const Display = styled.div`
 
   position: relative;
   padding: 25px 0;
+
+  ${({ theme }) => theme.mediaQueries[MOBILE_MENU_MEDIA_QUERY]`
+    width: calc(100% + ${HEADER_PADDING_MOBILE * 2}px);
+    left: -${HEADER_PADDING_MOBILE}px;
+
+    & > :first-child {
+      margin-left: ${HEADER_PADDING_MOBILE}px;
+    }
+  `}
 `;
 
 const MiscContentContainer = styled.span`
@@ -133,14 +150,15 @@ const landingCardStyles = css`
   width: 350px;
   height: 180px;
   flex-shrink: 0;
-  margin-left: 22px;
+  flex-grow: 0;
+  margin-right: 22px;
 
-  &:first-child {
+  &:last-child {
     margin-left: 0;
   }
 
   ${({ theme }) => theme.mediaQueries.largeMobile`
-    width: 100%;
+    width: 80%;
   `}
 `;
 
