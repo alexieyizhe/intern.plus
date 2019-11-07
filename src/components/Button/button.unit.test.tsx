b6fc6f90194edd30570f72bd443c6bec449962e8
Alex Xie <alexieyizhe@gmail.com>
2019-11-07 14:08:01 -0500

    Get basic component mounting working

---
+++ src/components/Button/button.unit.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { withTheme } from "../../theme/withTheme";
+
+import Button from "./index";
+
+describe("Button component", () => {
+  it("works", () => {
+    cy.mount(withTheme(<Button color="greenDark">Sup</Button>));
+    cy.contains("Sup");
+  });
+});
