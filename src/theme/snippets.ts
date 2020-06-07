507d5c7a55415a1ab79bb2613ea9aa56b13fbcca
Alex Xie <alexieyizhe@gmail.com>
2020-06-07 17:22:54 -0400

    Adjust hoverr styles for cards

--- src/theme/snippets.ts
+++ src/theme/snippets.ts
@@ -12,26 +12,11 @@ export const spin = keyframes`
 
 export const hoverStyles = css`
   cursor: pointer;
-  &::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-    width: 100%;
-    height: 100%;
+  transition: opacity 200ms;
 
-    border-radius: ${({ theme }) => theme.borderRadius.large}px;
-    box-shadow: ${({ theme }) => theme.boxShadow.hover};
-
-    transition: opacity 100ms ease-in;
-    opacity: 0;
-    backface-visibility: hidden; /* for this issue: https://stackoverflow.com/questions/11045451/white-flash-blink-on-hover-with-chrome */
-  }
-
-  &:hover::after,
-  &:focus::after {
-    opacity: 1;
+  &:hover,
+  &.focus-visible {
+    opacity: 0.7;
   }
 `;
 
