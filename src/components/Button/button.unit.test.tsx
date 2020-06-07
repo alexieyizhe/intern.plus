4a1f88c7e38a533345bc16fd421e85703741bb71
Alex Xie <alexieyizhe@gmail.com>
2020-06-07 16:49:51 -0400

    Clean up

--- src/components/Button/button.unit.test.tsx
+++ src/components/Button/button.unit.test.tsx
@@ -1,15 +1,20 @@
 /// <reference types="cypress" />
 
 import React from "react";
-import { mount } from "cypress-react-unit-test";
 
-import Button from "./index";
-import { withTheme } from "../../theme/cypress/withTheme";
+import Button from ".";
+import { mountWithTheme } from "../../theme/cypress/utils";
 
 describe("Button component", () => {
   it("renders properly", () => {
-    mount(withTheme(<Button color="greenPrimary">Sup</Button>));
+    mountWithTheme(<Button>Sup</Button>);
+
     cy.get("button").contains("Sup");
+  });
+
+  it("renders theme colors", () => {
+    mountWithTheme(<Button color="greenPrimary">Hello</Button>);
+
     cy.get("button")
       .should("have.css", "background-color")
       .and("eq", "rgb(80, 117, 97)");
