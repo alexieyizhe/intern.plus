a24fac0eb227d23f35de8e90d3bbf487ddd3819c
Alex Xie <alexieyizhe@gmail.com>
2020-10-24 21:29:22 -0400

    Hotfix force light mode

--- src/contexts/theme.tsx
+++ src/contexts/theme.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useMemo } from "react";
+import React, { createContext, useContext, useEffect, useMemo } from "react";
 import { ThemeProvider } from "styled-components";
 import useDarkMode from "use-dark-mode";
 
@@ -21,9 +21,13 @@ export const SiteThemeContext: React.Context<ISiteThemeState> = createContext(
 );
 
 export const SiteThemeContextProvider: React.FC = ({ children, ...rest }) => {
-  const { value: isDarkMode, toggle: toggleDarkMode } = useDarkMode(false, {
-    storageKey: undefined,
-  });
+  const { value: isDarkMode, toggle: toggleDarkMode } = useDarkMode();
+
+  useEffect(() => {
+    if (isDarkMode) {
+      toggleDarkMode();
+    }
+  }, [isDarkMode, toggleDarkMode]);
 
   const curMode = useMemo<ThemeMode>(() => (isDarkMode ? "dark" : "light"), [
     isDarkMode,
