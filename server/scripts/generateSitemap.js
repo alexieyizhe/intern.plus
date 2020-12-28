54c291d1e3babf84f3075b68ec3604858b44385c
Alex Xie <alexieyizhe@gmail.com>
2020-12-27 20:08:26 -0500

    purge old server code and add new github workflows

--- server/scripts/generateSitemap.js
+++
@@ -1,6 +0,0 @@
-require("babel-register");
-
-const router = require("../../src/App.tsx").default;
-const Sitemap = require("../").default;
-
-new Sitemap(router).build("http://intern.plus").save("./sitemap.xml");
