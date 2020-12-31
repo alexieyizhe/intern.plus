e5cafdf8dcff611d4a047fca53ad4233330b267e
Alex Xie <alexieyizhe@gmail.com>
2020-12-31 18:03:01 -0500

    add normalized db definition and run migrations

--- backend/internplus_backend/api/models.py
+++ backend/internplus_backend/api/models.py
@@ -1,96 +1,57 @@
 from django.db import models
 
 
-class Location(models.Model):
+class CommonModel(models.Model):
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Location(CommonModel):
     city = models.CharField(max_length=100)
-    subdivision = models.CharField(max_length=100)
+    subdivision = models.CharField(max_length=100, null=True)
     country = models.CharField(max_length=100)
 
 
-class Tag(models.Model):
+class Tag(CommonModel):
+    value = models.CharField(max_length=100)
+
+
+class HourlySalary(CommonModel):
+    value = models.PositiveIntegerField()  # normalized to HOURLY value
+    currencyCode = models.CharField(max_length=3)
+
+
+class Company(CommonModel):
+    slug = models.SlugField(max_length=100, unique=True)
     name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    website = models.URLField()
+    logo = models.ImageField()
 
 
-class Company(models.Model):
+class Job(CommonModel):
+    slug = models.SlugField(max_length=100, unique=True)
     name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
-    description = models.CharField(max_length=500)  # used to be `desc`
-    link = models.URLField()  # used to be `websiteUrl`
-    logo = models.URLField()  # used to be `logoSrc`
-
-    numReviews = models.IntegerField()  # used to be `numRatings``
-
-    totRating = models.DecimalField(max_digits=11, decimal_places=2)
-    avgRating = models.FloatField()
-
-    avgLearningMentorshipRating = models.FloatField()
-    totLearningMentorshipRating = models.FloatField()
-    avgMeaningfulWorkRating = models.FloatField()
-    totMeaningfulWorkRating = models.FloatField()
-    avgWorkLifeBalanceRating = models.FloatField()
-    totWorkLifeBalanceRating = models.FloatField()
-
-    totHourlySalary = models.FloatField()
-    avgHourlySalary = models.FloatField()
-    minHourlySalary = models.FloatField()
-    maxHourlySalary = models.FloatField()
-    hourlySalaryCurrency = models.CharField(max_length=3)
-
-    def __str__(self):
-        return self.name
-
-
-class Job(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     locations = models.ManyToManyField(Location)
 
-    totRating = models.FloatField()
-    avgRating = models.FloatField()
 
-    avgLearningMentorshipRating = models.FloatField()
-    totLearningMentorshipRating = models.FloatField()
-    avgMeaningfulWorkRating = models.FloatField()
-    totMeaningfulWorkRating = models.FloatField()
-    avgWorkLifeBalanceRating = models.FloatField()
-    totWorkLifeBalanceRating = models.FloatField()
-
-    totHourlySalary = models.FloatField()
-    avgHourlySalary = models.FloatField()
-    minHourlySalary = models.FloatField()
-    maxHourlySalary = models.FloatField()
-    hourlySalaryCurrency = models.CharField(max_length=3)
-
-    def __str__(self):
-        return self.name
-
-
-class Rating(models.Model):
-    overall = models.IntegerField()
-
-
-class Review(models.Model):
-    class SalaryPeriod(models.TextChoices):
-        YEARLY = 'YEARLY'
-        WEEKLY = 'WEEKLY'
-        HOURLY = 'HOURLY'
-
+class Review(CommonModel):
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    body = models.CharField(max_length=100)
-    tags = models.ManyToManyField(Tag)
+
     author = models.EmailField()
 
-    overallRating = models.FloatField()
-    learningMentorshipRating = models.FloatField()
-    meaningfulWorkRating = models.FloatField()
-    workLifeBalanceRating = models.FloatField()
+    overall = models.PositiveIntegerField()
+    learningMentorship = models.PositiveIntegerField()
+    meaningfulWork = models.PositiveIntegerField()
+    workLifeBalance = models.PositiveIntegerField()
 
-    salary = models.FloatField()
-    salaryCurrency = models.CharField(max_length=3)
-    salaryPeriod = models.CharField(
-        max_length=20, choices=SalaryPeriod.choices)
-
-    def __str__(self):
-        return self.name
+    salary = models.OneToOneField(
+        HourlySalary, on_delete=models.CASCADE, null=True)
+    body = models.CharField(max_length=100)
+    tags = models.ManyToManyField(Tag, blank=True)
+    #  can add benefits, etc
