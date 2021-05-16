91d6d45ab624a1dd147a611472b4971e23790ae3
Alex Xie <alexieyizhe@gmail.com>
2021-05-16 11:44:42 -0400

    Clean up data and parse lookup join tables

--- api/seed/script.js
+++ api/seed/script.js
@@ -6,13 +6,10 @@ const COMPANIES = {};
 const JOBS = {};
 const REVIEWS = {};
 
-const COMPANY_JOBS = {};
-const COMPANY_REVIEWS = {};
-const JOB_REVIEWS = {};
-
 const companiesData = fs.readFileSync('./data-companies', 'utf8');
 const jobsData = fs.readFileSync('./data-jobs', 'utf8');
 const reviewsData = fs.readFileSync('./data-reviews', 'utf8');
+
 companiesData.split("\n").forEach(v => {
   const inside = v.match(/^\((.*)\)$/);
   try {
@@ -55,16 +52,55 @@ reviewsData.split("\n").forEach(v => {
   try {
     const matches = reviewsRegex.exec(v);
     const [original, reviewId, reviewCreatedAt, reviewUpdatedAt, reviewDeletedAt, , , reviewBody, reviewSalaryAmt, reviewSalaryCurrency, reviewSalaryPeriod, overallRating, learningMentorshipRating, , meaningfulWorkRating, , workLifeBalanceRating, , reviewTags, , isSpam, isLegacy, rest] = matches;
-    if(noContents(reviewId) || noContents(reviewCreatedAt) || noContents(reviewUpdatedAt) || noContents(reviewBody) || noContents(reviewSalaryAmt) || noContents(reviewSalaryCurrency) || noContents(reviewSalaryPeriod) || noContents(overallRating) || noContents(isSpam) || noContents(isLegacy)) {
+    if(noContents(reviewId) || noContents(reviewCreatedAt) || noContents(reviewUpdatedAt) || noContents(reviewBody) || noContents(reviewSalaryAmt) || noContents(reviewSalaryCurrency) || noContents(reviewSalaryPeriod) || noContents(overallRating) || noContents(isSpam) || noContents(isLegacy) || reviewSalaryCurrency.length !== 3) {
       console.log({original, reviewId, reviewCreatedAt, reviewUpdatedAt, reviewDeletedAt, reviewBody, reviewSalaryAmt, reviewSalaryCurrency, reviewSalaryPeriod, overallRating, learningMentorshipRating, meaningfulWorkRating, workLifeBalanceRating, reviewTags, isSpam, isLegacy, rest});
     }
 
-    REVIEWS[reviewId] = {original, reviewId, reviewCreatedAt, reviewUpdatedAt, reviewDeletedAt, reviewBody, reviewSalaryAmt, reviewSalaryCurrency, reviewSalaryPeriod, overallRating, learningMentorshipRating, meaningfulWorkRating, workLifeBalanceRating, reviewTags, isSpam, isLegacy, rest};
+    REVIEWS[reviewId] = {original, reviewId, reviewCreatedAt, reviewUpdatedAt, reviewDeletedAt, reviewBody: reviewBody.replaceAll("\\'", "'").replaceAll('\\"', '"').replaceAll("\\n", "\n"), reviewSalaryAmt, reviewSalaryCurrency, reviewSalaryPeriod, overallRating, learningMentorshipRating, meaningfulWorkRating, workLifeBalanceRating, reviewTags, isSpam, isLegacy, rest};
 
   } catch(e) {
     console.error(e, v);
   }
 })
+
+
+const REVIEW_TO_JOB = {};
+const JOB_TO_COMPANY = {};
+
+const reviewsToJobsData = fs.readFileSync('./data-join-jobs-reviews', 'utf8');
+const jobsToCompaniesData = fs.readFileSync('./data-join-companies-jobs', 'utf8');
+
+const joinMatchRegex = /\('([a-zA-Z0-9]{25})','([a-zA-Z0-9]{25})','.{55}'\)/g;
+const rtjMatches = [...reviewsToJobsData.matchAll(joinMatchRegex)];
+rtjMatches.forEach(match => {
+  const [originalRtj, reviewId, correspondingJobId] = match;
+
+  const review = REVIEWS[reviewId];
+  const correspondingJob = JOBS[correspondingJobId];
+
+  if(!review || !correspondingJob) {
+    console.error("No match for review", reviewId, "to job", correspondingJobId);
+  }
+})
+
+const jtcMatches = [...jobsToCompaniesData.matchAll(joinMatchRegex)];
+jtcMatches.forEach(match => {
+  const [originalJtc, jobId, correspondingCompanyId] = match;
+
+  const job = JOBS[jobId];
+  const correspondingCompany = COMPANIES[correspondingCompanyId];
+
+  if(!job || !correspondingCompany) {
+    console.error("No match for job", jobId, "to company", correspondingCompanyId);
+  }
+})
+
+
+
+// build review-job lookup
+// build job-company lookup
+// iterate reviews, add to jobs
+// iterate jobs, add to company
 
 
 console.log(Object.keys(COMPANIES).length)
