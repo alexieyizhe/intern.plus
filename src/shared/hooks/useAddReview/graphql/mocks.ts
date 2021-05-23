189d6bd468dd57648913d0c04e4030acf5a43e92
Alex Xie <alexieyizhe@gmail.com>
2021-05-22 22:21:39 -0400

    Fix build and clean up

--- src/shared/hooks/useAddReview/graphql/mocks.ts
+++
@@ -1,5 +0,0 @@
-export const mockAddReview = () => ({
-  reviewCreate: {
-    id: "review-added",
-  },
-});
--- src/shared/hooks/useSearchQueryDef.ts
+++ src/shared/hooks/useSearchQueryDef.ts
@@ -1,7 +1,5 @@
-import { useMemo } from "react";
 import { DocumentNode } from "graphql";
 
-import { useSearchParams } from "src/shared/hooks/useSearchParams";
 import { SearchType, SearchSort } from "src/shared/constants/search";
 
 export interface ISearchQueryBuilderOptions {
